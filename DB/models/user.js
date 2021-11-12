a3049a384242c930c5cf6599486e67862db7fc7f
Dante Allievi <danteallievi@gmail.com>
2021-11-12 20:44:11 +0100

    User schema added and model edited

--- DB/models/user.js
+++ DB/models/user.js
@@ -13,6 +13,15 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  age: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      "https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-user-14.png",
+  },
   friends: [
     {
       type: [Types.ObjectId],
@@ -25,15 +34,6 @@ const userSchema = new Schema({
       ref: "User",
     },
   ],
-  age: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    default:
-      "https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-user-14.png",
-  },
 });
 
 const User = model("User", userSchema, "Users");
